e180da4ade5c9ad388b32aa0d53f6737554f5306
Jonathan Rainer <jr776@york.ac.uk>
2018-01-05 08:46:53 +0000

    Made all of the structs packed such that it's easier to decode the output later.

--- rtl/ryuki_datatypes.sv
+++ rtl/ryuki_datatypes.sv
@@ -2,41 +2,41 @@
 
 package ryuki_datatypes;
 
-    typedef struct {
+    typedef struct packed {
         integer time_start;
         integer time_end;
     } mem_access_req;
     
-    typedef struct {
+    typedef struct packed {
             integer time_start;
             integer time_end;        
     } mem_access_res;
 
-    typedef struct {
+    typedef struct packed {
         integer time_start;
         integer time_end;
         mem_access_req mem_access_req;
         mem_access_res mem_access_res;
     } IF_data;
     
-    typedef struct {
+    typedef struct packed {
         integer time_start;
         integer time_end;
     } ID_data;
     
-    typedef struct {
+    typedef struct packed {
         integer time_start;
         integer time_end;
         mem_access_req mem_access_req;
     } EX_data;
     
-    typedef struct {
+    typedef struct packed {
         integer time_start;
         integer time_end;
         mem_access_res mem_access_res;
     } WB_data;
     
-    typedef struct {
+    typedef struct packed {
         bit [`DATA_WIDTH-1:0] instruction;
         bit [`ADDR_WIDTH-1:0] addr;
         bit pass_through;
